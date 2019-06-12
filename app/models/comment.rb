1b677b46030564bee8f64fa07188c7ad386ae453
Peter Viss <peter.c.viss@biola.edu>
2019-06-12 08:20:22 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+
+  def user_attributes=(user_attribute)
+    if !user_attribute[:username].empty?
+    user_attribute.values.each do |user_att|
+      user = User.find_or_create_by(username: user_att)
+      self.user = user
+      end
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+   accepts_nested_attributes_for :categories
 
+
+
+
+  def category_attributes=(category_attribute)
+    category_attribute.values.each do |cat_attr|
+      category = Category.find_or_create_by(cat_attr)
+        self.categories << category
+      end
+    end
 
 end
