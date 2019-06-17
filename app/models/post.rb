dc3dfa56db9826d4f080013fb48dddcf07eb3b33
Meghan Miller <mille265@gmail.com>
2019-06-17 09:56:08 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,21 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
+
+def get_uniq_commentors
+  return self.users.uniq
+end
+
+def categories_attributes=(categories_attributes)
+  
+  categories_attributes.values.each do |categories_attribute|
+    if !categories_attribute["name"].empty?
+    category = Category.find_or_create_by(categories_attribute)
+    self.categories << category
+    end
+  end
+end
 
 
 end
